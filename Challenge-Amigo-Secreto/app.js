2d5f99c24e6981f8df325d0348323c9fe9304911
Roony-6 <roony7.rrc@gmail.com>
2025-08-07 20:40:39 -0600

    Version final. Challenge Amigo Secreto

--- Challenge-Amigo-Secreto/app.js
+++ Challenge-Amigo-Secreto/app.js
@@ -1,1 +1,51 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+
+
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+    if (nombre.trim() === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        limpiarCampo('amigo');
+        return;
+    } else if (amigos.includes(nombre)) {
+        alert("Tu amigo ya se encuentra agregado");
+        limpiarCampo('amigo');
+        return;
+    } else {
+        amigos.push(nombre);
+        limpiarCampo('amigo');
+    }
+    console.log(amigos)
+    mostrarAmigos();
+    return;
+}
+
+function limpiarCampo(id){
+    document.querySelector(`#${id}`).value = '';
+}
+
+function mostrarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ""; // Limpiar la lista antes de agregar nuevos elementos
+
+    for (let i = 0; i < amigos.length; i++) {
+        listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+
+function sortearAmigo() {
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = ''; // Limpiar resultado anterior
+
+    if (amigos.length === 0) {
+        resultado.innerHTML = '<li>No hay amigos para sortear.</li>';
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    resultado.innerHTML = `<li>El amigo sorteado es: <strong>${amigoSorteado}</strong></li>`;
+}
